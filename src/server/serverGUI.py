d64088c84dae98c360eaec9b1842da6ca8d96f9f
alonbarak <alonbarak3110@gmail.com>
2022-02-23 18:21:27 +0200

    updated server GUI

--- src/server/serverGUI.py
+++ src/server/serverGUI.py
@@ -1,3 +1,4 @@
+import socket
 import threading
 from server import Server
 from tkinter import *
@@ -11,8 +12,13 @@ class serverGUI:
         self.Window = Tk()
 
         self.go = Button(self.Window, text="Start", command=lambda: self.start())
-        self.go.place(relx=0,
-                      rely=0)
+        self.go.place(relx=0.45,
+                      rely=0.2)
+        host_name = socket.gethostname()
+        host_add = socket.gethostbyname(host_name)
+        self.lable = Label(self.Window, text="Server IP Address : " + host_add)
+        self.lable.place(relx=0.10,
+                         rely= 0.5)
 
         self.Window.mainloop()
 
